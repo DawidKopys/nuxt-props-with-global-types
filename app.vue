5d8ea61184ba556891e05cc84a6e3e0ad35ea0a6
Dawid Kopys <dawid.kopys@creativestyle.pl>
2023-11-21 16:09:06 +0100

    Initial commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+  <!-- we should get ts errors for all components -->
+  <CompWithExplicitlyImportedType
+    :foo="{ bar: 123 }"
+  />
+  <CompUsingGlobalType1 
+    :foo="{ bar: 123 }"
+  />
+  <CompUsingGlobalType2
+    :foo="{ bar: 123 }"
+  />
+</template>
